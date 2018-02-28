9b549de7ffe7bf0b7c86972789a3cf15737abc0d
Trainee <Trainee@KGFSL.COM>
2018-02-28 15:30:52 +0530

    first commit

---
+++ src/main/java/com/kgisl/bout/BoutApplication.java
@@ -0,0 +1,22 @@
+package com.kgisl.bout;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/db/*");
+		return registration;
+	}
+}
